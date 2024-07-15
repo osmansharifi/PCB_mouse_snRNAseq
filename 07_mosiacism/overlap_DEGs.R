ee91b5b20ec80268b0ee1fabd907bb971c47e491
Osman <50931327+osmansharifi@users.noreply.github.com>
2024-07-15 15:44:46 -0700

    summary DEGs

--- 07_mosiacism/overlap_DEGs.R
+++ 07_mosiacism/overlap_DEGs.R
@@ -13,6 +13,59 @@ library(ggplot2)
 library(ggrepel)
 library(GeneOverlap)
 library(enrichR)
+library(readr)
+################
+## load files ##
+################
+# Set the working directory to the parent folder containing the subdirectories
+setwd("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/")
+
+# Get the list of directories
+exp1 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/1_AllcellsVsAllcells_from_MUTPCB_WTPCB/sig_DEGs_1_AllcellsVsAllcells_from_MUTPCB_WTPCB.csv", header = TRUE)
+exp2 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/2_AllcellsVsAllcells_from_MUTPCB_MUTVEHICLE/sig_DEGs_2_AllcellsVsAllcells_from_MUTPCB_MUTVEHICLE.csv", header = TRUE)
+exp3 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/3_WTcellsVsWTcells_from_WTPCB_WTVEHICLE/sig_WTcellsVsWTcells_from_WTPCB_WTVEHICLE_DEGs.csv", header = TRUE)
+exp4 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/4_WTcellsVsWTcells_from_MUTPCB_WTPCB/sig_DEGs_4_WTcellsVsWTcells_from_MUTPCB_WTPCB.csv", header = TRUE)
+exp5 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/5_WTcellsVsWTcells_from_MUTPCB_WTVEHICLE/sig_DEGs_5_WTcellsVsWTcells_from_MUTPCB_WTVEHICLE.csv", header = TRUE)
+exp6 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/6_AllcellsVsAllcells_from_MUTVEHICLE_WTVEHICLE/sig_DEGs_6_AllcellsVsAllcells_from_MUTVEHICLE_WTVEHICLE.csv", header = TRUE)
+exp7 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/7_WTcellsVsWTcells_from_MUTVEHICLE_WTVEHICLE/sig_DEGs_7_WTcellsVsWTcells_from_MUTVEHICLE_WTVEHICLE.csv", header = TRUE)
+exp8 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/8_WTcellsVsWTcells_from_MUTVEHICLE_WTPCB/sig_DEGs_8_WTcellsVsWTcells_from_MUTVEHICLE_WTPCB.csv", header = TRUE)
+exp9 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/9_WTcellsVsWTcells_from_HETPCB_HETVEHICLE/sig_WTcellsVsWTcells_from_HETPCB_HETVEHICLE_DEGs.csv", header = TRUE)
+exp10 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/10_MUTcellsVsMUTcells_from_HETPCB_HETVEHICLE/sig_MUTcellsVsMUTcells_from_HETPCB_HETVEHICLE_DEGs.csv", header = TRUE)
+exp11 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/11_WTcellsVsMUTcells_from_MUTPCB_MUTVEHICLE/sig_DEGs_11_WTcellsVsMUTcells_from_MUTPCB_MUTVEHICLE.csv", header = TRUE)
+exp12 <- read.csv("/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/12_MUTcellsVsWTcells_from_MUTPCB_MUTVEHICLE/sig_DEGs_12_MUTcellsVsWTcells_from_MUTPCB_MUTVEHICLE.csv", header = TRUE)
+
+# List of file paths
+file_paths <- c(
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/1_AllcellsVsAllcells_from_MUTPCB_WTPCB/sig_DEGs_1_AllcellsVsAllcells_from_MUTPCB_WTPCB.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/2_AllcellsVsAllcells_from_MUTPCB_MUTVEHICLE/sig_DEGs_2_AllcellsVsAllcells_from_MUTPCB_MUTVEHICLE.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/3_WTcellsVsWTcells_from_WTPCB_WTVEHICLE/sig_WTcellsVsWTcells_from_WTPCB_WTVEHICLE_DEGs.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/4_WTcellsVsWTcells_from_MUTPCB_WTPCB/sig_DEGs_4_WTcellsVsWTcells_from_MUTPCB_WTPCB.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/5_WTcellsVsWTcells_from_MUTPCB_WTVEHICLE/sig_DEGs_5_WTcellsVsWTcells_from_MUTPCB_WTVEHICLE.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/6_AllcellsVsAllcells_from_MUTVEHICLE_WTVEHICLE/sig_DEGs_6_AllcellsVsAllcells_from_MUTVEHICLE_WTVEHICLE.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/7_WTcellsVsWTcells_from_MUTVEHICLE_WTVEHICLE/sig_DEGs_7_WTcellsVsWTcells_from_MUTVEHICLE_WTVEHICLE.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/8_WTcellsVsWTcells_from_MUTVEHICLE_WTPCB/sig_DEGs_8_WTcellsVsWTcells_from_MUTVEHICLE_WTPCB.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/9_WTcellsVsWTcells_from_HETPCB_HETVEHICLE/sig_WTcellsVsWTcells_from_HETPCB_HETVEHICLE_DEGs.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/10_MUTcellsVsMUTcells_from_HETPCB_HETVEHICLE/sig_MUTcellsVsMUTcells_from_HETPCB_HETVEHICLE_DEGs.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/11_WTcellsVsMUTcells_from_MUTPCB_MUTVEHICLE/sig_DEGs_11_WTcellsVsMUTcells_from_MUTPCB_MUTVEHICLE.csv",
+  "/Users/osman/Documents/GitHub/PEBBLES_mouse_snRNAseq/07_mosiacism/12_MUTcellsVsWTcells_from_MUTPCB_MUTVEHICLE/sig_DEGs_12_MUTcellsVsWTcells_from_MUTPCB_MUTVEHICLE.csv"
+)
+
+# Read each file into a list of data frames
+exp_list <- lapply(file_paths, read.csv, header = TRUE)
+
+# Extract the filenames without extension
+file_names <- tools::file_path_sans_ext(basename(file_paths))
+
+# Add DEG_experiment column to each data frame
+for (i in seq_along(exp_list)) {
+  exp_list[[i]]$DEG_experiment <- file_names[i]
+}
+
+combined_df <- do.call(rbind, exp_list)
+
+# Write the concatenated data frame to a new CSV file
+write_csv(concatenated_df, "concatenated_data.csv")
+
 ###########################################
 ## Venn diagram of the overlapping genes ##
 ###########################################
